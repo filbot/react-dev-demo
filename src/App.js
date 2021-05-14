37a944b37ca61f2a370a23295cb4b829436c630f
Filip Grace <filip@filbot.com>
2021-05-14 07:13:35 -0700

    Clean up empty spaces

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {ThemeProvider, useTheme} from './components/ThemeContext';
 import Header from './components/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Footer from './components/Footer';
 import About from './components/About';
-import {useTheme} from './components/ThemeContext';
 
 function App() {
-  const [showForm, setShowForm] = useState(true);
+  const [showForm, setShowForm] = useState(false);
   const [todos, setTodos] = useState([]);
   const darkTheme = useTheme();
 
@@ -37,7 +37,6 @@ function App() {
     await fetch(`http://localhost:3001/todos/${id}`, {
       method: 'DELETE'
     })
-
     setTodos(todos.filter((todo) => todo.id !== id))
   }
 
@@ -52,7 +51,6 @@ function App() {
       body: JSON.stringify(updatedTodo)
     })
     const data = await resp.json();
-
     setTodos(todos.map((todo) => todo.id === id ? {...todo, flag: data.flag} : todo))
   }
 
@@ -64,26 +62,25 @@ function App() {
       },
       body: JSON.stringify(todo)
     })
-
     const data = await resp.json();
-
-
     setTodos([...todos, data]);
   }
 
   return (
     <Router>
-      <div className={darkTheme ? "App dark" : "App"}>
-        <Header onAdd={() => setShowForm(!showForm)} />
-        <Route path='/' exact render={(props) => (
-          <>
-            {showForm && <AddTodo onAdd={addTodo} />}
-            {todos.length > 0 ? <Todos todos={todos} onDelete={deleteTodo} onToggle={toggleFlag} /> : 'Nothing but time on your hands'}
-          </>
-        )} />
-        <Route path='/about' component={About} />
-        <Footer />
-      </div>
+      <ThemeProvider>
+        <div className={darkTheme ? "App dark" : "App"}>
+          <Header onAdd={() => setShowForm(!showForm)} />
+          <Route path='/' exact render={(props) => (
+            <>
+              {showForm && <AddTodo onAdd={addTodo} />}
+              {todos.length > 0 ? <Todos todos={todos} onDelete={deleteTodo} onToggle={toggleFlag} /> : <div className="empty-todo-list">Nothing but time on your hands</div>}
+            </>
+          )} />
+          <Route path='/about' component={About} />
+          <Footer />
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }

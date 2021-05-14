ba46e0671960102bfc621851bd5263e12e1a1a1e
Filip Grace <filip@filbot.com>
2021-05-13 18:11:54 -0700

    Add about component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom';
+
+const About = () => {
+    return (
+        <>
+            <h1>This is a todo app demo, there are many like it, but this one is mine.</h1>
+            <Link to='/'>Go back</Link>
+        </>
+    )
+}
+
+export default About;

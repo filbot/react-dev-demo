09666a78c892d31b9442d051afd7d96ce467b8b4
Filip Grace <filip@filbot.com>
2021-05-13 19:32:59 -0700

    Create theme context provider

---
+++ src/components/ThemeContext.js
@@ -0,0 +1,29 @@
+import {useState, useContext, createContext} from 'react';
+
+const ThemeContext = createContext();
+const ThemeUpdateContext = createContext();
+
+// Custom hooks
+export const useTheme = () => {
+    return useContext(ThemeContext);
+}
+
+export const useThemeUpdate = () => {
+    return useContext(ThemeUpdateContext);
+}
+
+export const ThemeProvider = ({children}) => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    const toggleTheme = () => {
+        setDarkTheme(prevDarkTheme => !prevDarkTheme);
+    }
+
+    return (
+        <ThemeContext.Provider value={darkTheme}>
+            <ThemeUpdateContext.Provider value={toggleTheme}>
+                {children}
+            </ThemeUpdateContext.Provider>
+        </ThemeContext.Provider>
+    )
+}
